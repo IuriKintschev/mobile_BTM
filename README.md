3cfbd1d1aedebf0562ec1559d1866af3c0ecaa0f
IuriKintschev <jtc.iuri07@gmail.com>
2020-06-22 17:55:39 -0400

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Behind The Masks
+
+<b> Aplicaçao versão mobile, utilizando `Flutter` </b>
+
+## `Em desenvolvimento!`
---
+++ android/app/src/main/kotlin/com/kintschev/mobile/btm/mobile_BTM/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kintschev.mobile.btm.mobile_BTM
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
